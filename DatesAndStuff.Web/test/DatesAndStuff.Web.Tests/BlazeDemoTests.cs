071c8b2fa8e517eb526851fcb6cb184b70aace5f
Otto Lengyel <ottolengyel0404@gmail.com>
2026-05-06 12:34:21 +0300

    4th part

---
+++ DatesAndStuff.Web/test/DatesAndStuff.Web.Tests/BlazeDemoTests.cs
@@ -0,0 +1,112 @@
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace DatesAndStuff.Web.Tests;
+
+[TestFixture]
+public class BlazeDemoTests
+{
+    private IWebDriver driver;
+    private const string BaseURL = "https://blazedemo.com";
+    private bool acceptNextAlert = true;
+
+    private Process? _blazorProcess;
+
+    [OneTimeSetUp]
+    public void StartBlazorServer()
+    {
+        var webProjectPath = Path.GetFullPath(Path.Combine(
+            Assembly.GetExecutingAssembly().Location,
+            "../../../../../../src/DatesAndStuff.Web/DatesAndStuff.Web.csproj"
+            ));
+
+        var webProjFolderPath = Path.GetDirectoryName(webProjectPath);
+
+        var startInfo = new ProcessStartInfo
+        {
+            FileName = "dotnet",
+            //Arguments = $"run --project \"{webProjectPath}\"",
+            Arguments = "dotnet run --no-build",
+            WorkingDirectory = webProjFolderPath,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false
+        };
+
+        _blazorProcess = Process.Start(startInfo);
+
+        // Wait for the app to become available
+        var client = new HttpClient();
+        var timeout = TimeSpan.FromSeconds(30);
+        var start = DateTime.Now;
+
+        while (DateTime.Now - start < timeout)
+        {
+            try
+            {
+                var result = client.GetAsync(BaseURL).Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    break;
+                }
+            }
+            catch (Exception)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+
+    [OneTimeTearDown]
+    public void StopBlazorServer()
+    {
+        if (_blazorProcess != null && !_blazorProcess.HasExited)
+        {
+            _blazorProcess.Kill(true);
+            _blazorProcess.Dispose();
+        }
+    }
+
+    [SetUp]
+    public void SetupTest()
+    {
+        driver = new ChromeDriver();
+    }
+
+    [TearDown]
+    public void TeardownTest()
+    {
+        try
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+        catch (Exception)
+        {
+            // Ignore errors if unable to close the browser
+        }
+    }
+
+    [Test]
+    public void Demo_CheckFlights_AtLeastThree()
+    {
+        driver.Navigate().GoToUrl(BaseURL);
+
+        new SelectElement(driver.FindElement(By.Name("fromPort"))).SelectByText("Mexico City");
+        new SelectElement(driver.FindElement(By.Name("toPort"))).SelectByText("Dublin");
+        driver.FindElement(By.XPath("//input[@value='Find Flights']")).Click();
+
+        var rows = driver.FindElements(By.XPath("//table[@class='table']/tbody/tr"));
+
+        int flightCount = rows.Count;
+
+        flightCount.Should().BeGreaterThanOrEqualTo(3);
+    }
+}
